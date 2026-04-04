42228494b13954853587b0a8c889ca5811e85962
Eduardo Leal Aruth <101681441+EduLeal@users.noreply.github.com>
2026-04-03 23:15:19 -0300

    feat: fetch

--- src/routes/watch-game-page.jsx
+++ src/routes/watch-game-page.jsx
@@ -1,7 +1,41 @@
 import { Link, useParams } from "react-router";
+import { useState } from "react";
 
 export function WatchGamePage() {
   const { id } = useParams();
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  /**
+   * Busca um estado aleatório de jogo
+   */
+  const fetchGame = async () => {
+    setError(false);
+    setLoading(true);
+    
+    try {
+        const response = await fetch(
+            `https://pi5-api-production.up.railway.app/api/v1/games/mock-state`, 
+            {
+                method: 'POST',
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error('Erro ao buscar estado do jogo');
+        }
+
+        const game = await response.json();
+        setData(game);
+        
+    } catch (err) {
+        setError(true);
+    } finally {
+        setLoading(false);
+    }
+   };
 
   return (
     <div>
