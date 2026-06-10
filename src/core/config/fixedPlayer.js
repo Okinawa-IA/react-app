e3cd0a3911c84669a7509e81c01ae1fc56c9cd62
Leonardo Lima <87489820+leolima92@users.noreply.github.com>
2026-06-10 10:02:30 -0300

    Add files via upload

---
+++ src/core/config/fixedPlayer.js
@@ -0,0 +1,14 @@
+export const FIXED_PLAYER = {
+  group_name 'Okinawa',
+  ai_player_name 'okinawa_bot',
+  ai_player_avatar
+    'httpsi0.wp.comblog.janm.orgwp-contentuploads201507shisa-by-troy-williams-via-flickr.jpgssl=1',
+  ai_player_description 'Bot de testes',
+  ai_player_move_endpoint 'httpsback-end-production-f7ba.up.railway.appmove',
+  id 114,
+  games_played 0,
+  games_won 0,
+  games_lost 0,
+  average_move_time null,
+  player_access_token 'BR4KsPzKaVryNyNeKHfxqBO2eaAg1_rvAwUsq6zci04',
+};
